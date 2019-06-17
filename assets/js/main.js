0481591819202d2dc0db3b98c77d140e299fefa9
Maurício Mutte <litiobr@gmail.com>
2019-06-16 22:28:21 -0300

    Script to get datas

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const $input = document.querySelector('.input');
+const $btn = document.querySelector('.btn');
+const dataStructure = {};
+let inputValue;
+
+$btn.addEventListener('click', getInputValue);
+$input.addEventListener('keyup', function(e) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    getInputValue();
+   }
+});
+
+function getInputValue() {
+  inputValue = $input.value.match(/\w+(,\w+)?/g)
+  console.log(inputValue)
+}
