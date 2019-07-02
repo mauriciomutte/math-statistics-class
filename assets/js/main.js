4eb13289022e30ac55a5b0344df7fb6641f6e4c5
Maurício Mutte <litiobr@gmail.com>
2019-07-02 18:07:28 -0300

    Fix numbers var

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const $send = document.querySelector('[data-js="send"]');
 const $removeSend = document.querySelector('[data-js="remove"]');
 const $calculate = document.querySelector('[data-js="calculate"]');
 const $viewNumber = document.querySelector('[data-js="enter-numbers"]');
-let numbers = [2,4,4,5,6];
+let numbers = [];
 let rol;
 
 $send.addEventListener('click', function() {
