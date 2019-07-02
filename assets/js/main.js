b447223963796339067536fb357cc74191c31349
Maurício Mutte <litiobr@gmail.com>
2019-07-02 15:59:28 -0300

    Add btn to remove last enter number

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const $input = document.querySelector('[data-js="input"]');
 const $send = document.querySelector('[data-js="send"]');
+const $removeSend = document.querySelector('[data-js="remove"]');
 const $viewNumber = document.querySelector('[data-js="enter-numbers"]');
 let numbers = [];
 
@@ -7,4 +8,9 @@ $send.addEventListener('click', function() {
   numbers.push( Number($input.value) );
   $viewNumber.innerHTML = numbers.join(', ');
   console.log(numbers);
+});
+
+$removeSend.addEventListener('click', function() {
+  numbers.pop();
+  $viewNumber.innerHTML = numbers.join(', ');
 });
